a641fccb6af30ccde0ad0fb53e4699044f27c6ad
Dan Zonca <ZoncaD@users.noreply.github.com>
2026-03-07 14:38:20 -0500

    Improved performace for index-based LazyWebElements

--- src/main/java/LazyWebElement.java
+++ src/main/java/LazyWebElement.java
@@ -1,19 +1,41 @@
-import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.WeakHashMap;
 
 public class LazyWebElement implements WebElement {
     private final WebDriver driver;
     private final By locator;
-    private final Integer index;
     private final LazyWebElement parent;
+
+    private static final WeakHashMap<WebElementListIdentifier, List<WebElement>> instanceLists = new WeakHashMap<>();
+    private final WebElementListIdentifier listIdentifier;
+    private final Integer index;
+
     private WebElement instance;
 
     public LazyWebElement(WebDriver driver, LazyWebElement parent, By locator, Integer index) {
-        if (index != null && index < 1) throw new IllegalArgumentException("Argument 'index' must be either null or a positive integer");
+        if (index != null) {
+            if (index < 1){
+                throw new IllegalArgumentException("Argument 'index' must be either null or a positive integer");
+            }
+            else {
+                // We must reuse the exact identifier object already in use, if one exists, to avoid memory leak.
+                final WebElementListIdentifier newIdentifier = new WebElementListIdentifier(parent, locator);
+                Optional<WebElementListIdentifier> exactKeyObj = instanceLists.keySet().stream()
+                        .filter(key -> key.equals(newIdentifier))
+                        .findFirst();
+                listIdentifier = exactKeyObj.orElse(newIdentifier);
+            }
+        }
+        else {
+            listIdentifier = null;
+        }
+
         this.driver = driver;
         this.locator = locator;
         this.parent = parent;
@@ -43,7 +65,7 @@ public class LazyWebElement implements WebElement {
         return freshInstance;
     }
 
-    private WebElement getNewInstanceWithIndex() {
+    private WebElement getNewInstanceFromList() {
         List<WebElement> matches;
         if (parent == null) {
             matches = driver.findElements(locator);
@@ -52,19 +74,33 @@ public class LazyWebElement implements WebElement {
             matches = parent.getInstance().findElements(locator);
         }
 
+        instanceLists.put(listIdentifier, matches);
+
         if (matches.size() < index) {
-            throw new NoSuchElementException("No element with index " + index + " found");
+            throw new NoSuchElementException("No element with index " + index + " found for locator " + locator);
         }
 
         return matches.get(index - 1);
     }
 
-    private WebElement getInstance() {
+    WebElement getInstance() {
         if (instance == null || Boolean.TRUE.equals(ExpectedConditions.stalenessOf(instance).apply(driver))) {
-            instance = index == null ? getNewInstance() : getNewInstanceWithIndex();
+            instance = listIdentifier == null ? getNewInstance() : getNewInstanceFromList();
         }
 
         return instance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LazyWebElement that = (LazyWebElement) o;
+        return Objects.equals(locator, that.locator) && Objects.equals(parent, that.parent) && Objects.equals(index, that.index);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(locator, parent, index);
     }
 
     @Override
@@ -175,5 +211,27 @@ public class LazyWebElement implements WebElement {
     @Override
     public <X> X getScreenshotAs(OutputType<X> target) throws WebDriverException {
         return getInstance().getScreenshotAs(target);
+    }
+
+    private static class WebElementListIdentifier {
+        public final LazyWebElement parent;
+        public final By locator;
+
+        public WebElementListIdentifier(LazyWebElement parent, By locator) {
+            this.parent = parent;
+            this.locator = locator;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            WebElementListIdentifier that = (WebElementListIdentifier) o;
+            return Objects.equals(parent, that.parent) && Objects.equals(locator, that.locator);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(parent, locator);
+        }
     }
 }
